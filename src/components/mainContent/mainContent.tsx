d7dde47bfcc4e6fe268aba25be298f64e709d924
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-02 22:23:19 +0300

    Добавил компонент на главную страницу
    Компонент "О себе"

---
+++ src/components/mainContent/mainContent.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ProjectSlider from '../projectSlider/projectSlider'
+
+import './mainContent.scss'
+
+const MainContent = () => {
+  return (
+    <section className='main__content'>
+      <div className='main__about'>
+        <div className='main__about-greeting'>Hello! I am</div>
+        <div className='main__about-title'>Aleksey Machulskiy</div>
+        <div className='main__about-status'>Junior front-end developer</div>
+        <div className='main__about-code'>
+          <div className='about__code-title'>
+            // write the code below to continue:
+          </div>
+          <div className='about__code-input'>
+            <span>˃</span> whois
+          </div>
+        </div>
+      </div>
+      <div className='main__app'>
+        <ProjectSlider />
+      </div>
+    </section>
+  )
+}
+
+export default MainContent
