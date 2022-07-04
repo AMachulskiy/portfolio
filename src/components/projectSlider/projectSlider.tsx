3bc64fa4d5522a06a6ac843709d89e56517e7d39
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-04 21:15:35 +0300

    Удалил лишний контент

--- src/components/projectSlider/projectSlider.tsx
+++ src/components/projectSlider/projectSlider.tsx
@@ -23,23 +23,6 @@ const ProjectSlider: React.FC = () => {
             <div className='slide__button'>see the project</div>
           </div>
         </div>
-        <div className='slide'>
-          <div className='slide__header'>
-            <div className='slide__title'>Project 2</div>
-            <div className='slide__desc'>4, 592 commits</div>
-          </div>
-          <div className='slide__content'>
-            <div className='slide__content-txt'>{'/**'}</div>
-            <div className='slide__content-text'>
-              <p>* Integrated 4 different APIs</p>
-              <p>* Over 200 different components</p>
-              <p>* React, Redux</p>
-              <p>* Full storybook setup</p>
-              <p>*/</p>
-            </div>
-            <div className='slide__button'>see the project</div>
-          </div>
-        </div>
       </div>
     </div>
   )
