f476b290aa8beb4b305afa25faaa4519c629ff7a
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-05 09:59:24 +0300

    Добавил страницу Home

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Typewriter from 'typewriter-effect'
+import Cards from '@src/components/cards/cards'
+
+import './home.scss'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div className='home'>
+        <div className='home__greeting'>Hello! I am</div>
+        <div className='home__title'>Aleksey Machulskiy</div>
+        <div className='home__status'>Junior front-end developer</div>
+        <div className='home__code'>
+          <div className='home__code-title'>
+            portfolio{'>'} <span>npx</span> create-react-app portfolio
+          </div>
+          <div className='home__code-input'>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter.pauseFor(3500).typeString('Happy hacking!').start()
+              }}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='home__app'>
+        <div className='home__app-blur' />
+        <Cards />
+      </div>
+    </>
+  )
+}
+
+export default Home
