7d91c9a02229a64a2c333a8acd94ea448577b1cf
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-05 09:59:01 +0300

    Выделил отдельно компонент контента вкладок

---
+++ src/components/Content/content.tsx
@@ -0,0 +1,14 @@
+import Home from '@src/pages/home/home'
+import React from 'react'
+
+import './content.scss'
+
+const Content: React.FC = () => {
+  return (
+    <section className='content'>
+      <Home />
+    </section>
+  )
+}
+
+export default Content
