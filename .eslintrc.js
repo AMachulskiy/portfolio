f8c78b26946fb7c1c9e0f0ccbce280df9e4e056d
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-10 16:47:47 +0300

    Добавил правила отключающие незначительные предупреждения

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,5 +41,7 @@ module.exports = {
     'react/require-default-props': 'off',
     '@typescript-eslint/ban-types': 'off',
     'react/prop-types': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
   },
 }
