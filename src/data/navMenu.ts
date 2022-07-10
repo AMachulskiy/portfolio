5e5a1663134df9a49dd6077f1b4ce99fbed9ebc6
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-10 16:51:04 +0300

    Создал интерфейс и файл данных для ссылок меню

---
+++ src/data/navMenu.ts
@@ -0,0 +1,9 @@
+import { INavMenu } from '@src/interfaces/INavMenu'
+
+const navMenu: INavMenu[] = [
+  { to: '/', title: '_home' },
+  { to: 'aboutme', title: '_about_me' },
+  { to: 'projects', title: '_projects' },
+]
+
+export default navMenu
---
+++ src/interfaces/INavMenu.ts
@@ -0,0 +1,4 @@
+export interface INavMenu {
+  to: string
+  title: string
+}
