53f9ba298ce0e8135335018af230fb458dd1f52b
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-10 16:49:16 +0300

    Добавил компонент для страницы 404

---
+++ src/pages/404/404.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './404.scss'
+
+const Page404: React.FC = () => {
+  return (
+    <section className='page404'>
+      <img src='../../img/ghost_red.png' alt='404' />
+      <h1>_404: The requested page does not exist...</h1>
+    </section>
+  )
+}
+
+export default Page404
