7ed0c55309b86709d32b6ce730f349d6997ded52
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-10 16:52:22 +0300

    Создал компонент для будущей страницы About me

---
+++ src/pages/about/aboutMe.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './aboutMe.scss'
+
+const AboutMe: React.FC = () => {
+  return (
+    <section className='aboutme'>
+      <img src='../../img/ghost_red.png' alt='404' />
+      <h1>The page is under development...</h1>
+    </section>
+  )
+}
+
+export default AboutMe
