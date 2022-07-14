a213a59043f17590d13a6216f859765b0eb83ec0
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-14 16:29:45 +0300

    Создал файлы для хранилища состояния

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+/* import { combineReducers, configureStore } from '@reduxjs/toolkit'
+
+const rootReducer = combineReducers({
+  accordeon: accordeonReducer,
+})
+
+const store = configureStore({
+  reducer: rootReducer,
+})
+
+export default store
+
+export type AppState = ReturnType<typeof rootReducer>
+export type AppStore = typeof store
+export type AppDispatch = AppStore['dispatch']
+*/
