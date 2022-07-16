c5e98573cd9e3febbafd425ec0d9a1139860aec1
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-16 11:31:48 +0300

    Добавил интерфейс и файл данных для страницы about

---
+++ src/data/myInterests.ts
@@ -0,0 +1,24 @@
+import { IMyInterests } from '@src/interfaces/IMyInterests'
+
+const myInterests: IMyInterests[] = [
+  { title: 'HTML' },
+  { title: 'CSS' },
+  { title: 'Java Script' },
+  { title: 'React JS' },
+  { title: 'Redux Toolkit' },
+  {
+    title: 'Free time',
+    item: [
+      '_nature walks.md',
+      '_roller skating.md',
+      '_cycling.md',
+      '_traveling by car.md',
+      '_ice skating.md',
+      '_snowboarding.md',
+      '_watching interesting movies.md',
+      '_listening to rock music.md',
+    ],
+  },
+]
+
+export default myInterests
---
+++ src/interfaces/IMyInterests.ts
@@ -0,0 +1,4 @@
+export interface IMyInterests {
+  title: string
+  item?: string[]
+}
