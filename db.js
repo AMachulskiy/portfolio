c74c5bb3499f6688f793a029278556045b01f6c6
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-17 14:54:20 +0300

    Создал файл локальной базы данных

---
+++ db.js
@@ -0,0 +1,24 @@
+module.exports = () => {
+  const data = {
+    projects: [],
+  }
+
+  data.projects.push(
+    {
+      id: 1,
+      title: '_landing page',
+      commits: 138,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      button: 'Check in GitHub',
+    },
+    {
+      id: 1,
+      title: '_portfolio',
+      commits: 245,
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+      button: 'Check in GitHub',
+    }
+  )
+
+  return data
+}
