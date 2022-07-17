0e6439c572b16db4b3a49c64234fe8f80dd083a5
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-17 14:57:51 +0300

    Добавил обертку Provider для всего приложения

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store/store'
 import App from './App'
 
 import '@styles/style.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 )
