aa2e8f47c5a01fd3b13c8728098a99a590d154d0
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-17 15:58:15 +0300

    Переписал код для вывода проектов из хранилища
    В процессе...

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,33 +1,48 @@
-import React from 'react'
+import { useAppDispatch, useAppSelector } from '@src/hooks/redux'
+import React, { useEffect, useState } from 'react'
 import Cards from '@src/components/cards/cards'
+import ProjectBtn from '@src/components/projectBtn/projectBtn'
+import getProjectsAction from '@src/store/projects/actions'
 
 import './projects.scss'
-import ProjectBtn from '@src/components/projectBtn/projectBtn'
+import { IProject } from '@src/interfaces/IProject'
+import Page404 from '../404/404'
 
 const Projects: React.FC = () => {
+  const [project, setProject] = useState<IProject[]>()
+  const dispatch = useAppDispatch()
+  const { data, isLoading, haveData } = useAppSelector(
+    (state) => state.projects
+  )
+
+  useEffect(() => {
+    if (data) {
+      setProject(data)
+    } else {
+      setProject(null)
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (!haveData) {
+      dispatch(getProjectsAction())
+    }
+  }, [dispatch])
+
+  if (isLoading) return <h1>_loading...</h1>
+  if (!data) return null
+  if (!project) return <Page404 />
+
   return (
     <section className='projects'>
       <div className='projects__title'>// Top projects</div>
-      <div className='project__wrap'>
-        <Cards title='Project 1' desc='245 commits' />
-        <div className='project__desc'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque
-          nisi ex, doloremque neque nam cupiditate ad, quaerat quasi, fugit
-          aliquid. Alias nobis odio cumque consequuntur sed autem laborum
-          eveniet!
+      {data.map(({ id, title, commits, desc, button }) => (
+        <div className='project__wrap'>
+          <Cards number={id} title={title} commits={commits} />
+          <div className='project__desc'>{desc}</div>
+          <ProjectBtn>{button}</ProjectBtn>
         </div>
-        <ProjectBtn>Check in GitHub</ProjectBtn>
-      </div>
-      <div className='project__wrap'>
-        <Cards title='Project 2' desc='138 commits' />
-        <div className='project__desc'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cumque
-          nisi ex, doloremque neque nam cupiditate ad, quaerat quasi, fugit
-          aliquid. Alias nobis odio cumque consequuntur sed autem laborum
-          eveniet!
-        </div>
-        <ProjectBtn>Check in GitHub</ProjectBtn>
-      </div>
+      ))}
     </section>
   )
 }
