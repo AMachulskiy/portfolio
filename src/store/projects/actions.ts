f6b00d7501a1276c2aa0c09e77f6d6a2e1ffdfd0
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-07-17 15:53:40 +0300

    Создал action для компонента проектов

---
+++ src/store/projects/actions.ts
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import ProjectService from '@src/services/projectService'
+
+const projectService = new ProjectService()
+
+const getProjectsAction = createAsyncThunk('projects', (arg, thunkApi) => {
+  return projectService.getProjects
+})
+
+export default getProjectsAction
