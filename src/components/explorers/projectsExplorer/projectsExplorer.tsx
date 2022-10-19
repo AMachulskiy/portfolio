334cc3a74cfd14401b309b1fafadcef2e605a9cb
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-10-19 12:56:25 +0300

    Добавил новый компонент страницы "Проекты"
    + переместил карточки проектов

--- src/components/explorers/projectsExplorer/projectsExplorer.tsx
+++ src/components/explorers/projectsExplorer/projectsExplorer.tsx
@@ -1,19 +1,25 @@
 import { ReactFC } from '@src/interfaces/react'
-import Projects from '@src/pages/projects/projects'
 import React from 'react'
+import Projects from './projects/projects'
 
 import './projectsExplorer.scss'
 
 const ProjectsExplorer: ReactFC = () => {
   return (
     <div className='project-explorer'>
-      <div className='project-explorer__sidebar'>1</div>
+      <div className='project-explorer__sidebar'>
+        <div className='checkbox'>
+          <label className='checkbox-elem'>
+            <input type='checkbox' />
+            <div />
+          </label>
+          <div className='checkbox-text'>HTML</div>
+        </div>
+      </div>
       <div className='project-explorer__content'>
         <Projects />
       </div>
-      <div className='project-explorer__elem'>
-        <div />
-      </div>
+      <div className='project-explorer__elem' />
     </div>
   )
 }
