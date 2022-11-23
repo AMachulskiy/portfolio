9f1ed6812df05c51c77ddfc590ead1f19d8858ef
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-11-23 12:22:59 +0300

    Добавил файл данных элементов фильтра по технологиям

---
+++ src/data/checkboxes.ts
@@ -0,0 +1,12 @@
+import { ICheckbox } from '@src/interfaces/filters'
+
+const technologies: ICheckbox[] = [
+  { id: 1, icon: 'FaHtml5', title: 'HTML', value: 'html' },
+  { id: 2, icon: 'IoLogoCss3', title: 'CSS', value: 'css' },
+  { id: 3, icon: 'IoLogoJavascript', title: 'JavaScript', value: 'javascript' },
+  { id: 4, icon: 'FaReact', title: 'React', value: 'react' },
+  { id: 5, icon: 'SiTypescript', title: 'TypeScript', value: 'typescript' },
+  { id: 6, icon: 'SiRedux', title: 'Redux', value: 'redux' },
+]
+
+export default technologies
