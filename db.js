a4b78cafbdff67600e4e19c30fb55a7ff81e7684
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-11-23 11:35:04 +0300

    Подготовил файл базы данных для фильтрации проектов

--- db.js
+++ db.js
@@ -1,4 +1,28 @@
 module.exports = () => {
+  const technologies = [
+    {
+      id: 1,
+      icon: '../../img/html5Logo.svg',
+      title: 'HTML | CSS | JS',
+      value: 'html',
+    },
+    { id: 2, icon: '../../img/reactLogo.svg', title: 'CSS', value: 'css' },
+    {
+      id: 3,
+      icon: '../../img/reactLogo.svg',
+      title: 'JavaScript',
+      value: 'javascript',
+    },
+    { id: 4, icon: '../../img/reactLogo.svg', title: 'React', value: 'react' },
+    {
+      id: 5,
+      icon: '../../img/reactLogo.svg',
+      title: 'TypeScript',
+      value: 'typescript',
+    },
+    { id: 6, icon: '../../img/reactLogo.svg', title: 'Redux', value: 'redux' },
+  ]
+
   const data = {
     projects: [],
   }
@@ -6,17 +30,27 @@ module.exports = () => {
   data.projects.push(
     {
       id: 1,
-      title: '_landing page',
-      commits: 138,
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      button: 'Check in GitHub',
+      title: 'Проект',
+      subtitle: '// _лендинг',
+      img: '../../img/xarizma-landing.jpg',
+      description: 'Сайт-лендинг для караоке клуба Xarizma.',
+      technology: technologies[0],
     },
     {
       id: 2,
-      title: '_portfolio',
-      commits: 245,
-      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-      button: 'Check in GitHub',
+      title: 'Проект',
+      subtitle: '// _портфолио',
+      img: '../../img/portfolio.jpg',
+      description: 'Мой сайт портфолио.',
+      technology: technologies[3],
+    },
+    {
+      id: 3,
+      title: 'Проект',
+      subtitle: '// _маркетплейс',
+      img: '../../img/marketplace.jpg',
+      description: 'Большой проект интернет магазина.',
+      technology: technologies[3],
     }
   )
 
