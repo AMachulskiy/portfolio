a2e74f66eb32461d792ac57a81303644bb90fb2e
Aleksey Machulskiy <aleksey.stelz@gmail.com>
2022-12-13 16:25:17 +0300

    Создал меню для мобильных устройств
    + адаптировал главный экран под разные разрешения экрана

--- src/components/navMenu/navMenu.tsx
+++ src/components/navMenu/navMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactFC } from '@src/interfaces/react'
 import { NavLink } from 'react-router-dom'
 import navMenu from '@src/data/navMenu'
@@ -6,14 +6,29 @@ import navMenu from '@src/data/navMenu'
 import './navMenu.scss'
 
 const NavMenu: ReactFC = () => {
+  const [isNavMobileShow, setIsNavMobileShow] = useState(false)
+
   return (
-    <nav className='nav'>
-      {navMenu.map(({ to, title }) => (
-        <NavLink className='nav__btn' key={to} to={to}>
-          {title}
-        </NavLink>
-      ))}
-    </nav>
+    <>
+      <nav className={`nav ${isNavMobileShow ? 'show__nav' : 'hide__nav'}`}>
+        {navMenu.map(({ to, title }) => (
+          <NavLink
+            className='nav__btn'
+            key={to}
+            to={to}
+            onClick={() => setIsNavMobileShow(!isNavMobileShow)}
+          >
+            {title}
+          </NavLink>
+        ))}
+      </nav>
+      <div
+        className='nav__btn-open'
+        onClick={() => setIsNavMobileShow(!isNavMobileShow)}
+      >
+        {!isNavMobileShow ? '_ открыть_навигацию' : '_закрыть_навигацию'}
+      </div>
+    </>
   )
 }
 
